127cb1b3d203743e453a6f67d6c22c4d10c9b432
mrsteamfist <christopher.wang@live.com>
2024-03-18 10:42:49 -0700

    Fixed movement system

--- Assets/Scripts/ControllerAPI.cs
+++ Assets/Scripts/ControllerAPI.cs
@@ -1,20 +1,26 @@
 using RPGM.Gameplay;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SearchService;
+using UnityEngine.Rendering.Universal;
 
 namespace Graveyard
 {
     public class ControllerAPI : MonoBehaviour
     {
+        public PixelPerfectCamera SceneCamera;
         RPGGraveyard controls;
-        public CharacterController2D characerController;
+        private Vector2 nextMoveCommand;
+        public float moveSpeed = 5f;
+        public Transform characterPosition;
+        public SpriteRenderer spriteRenderer;
+        public Sprite bottomFacing;
+        public Sprite leftFacing;
+        public Sprite topFacing;
 
         private void Awake()
         {
             controls = new RPGGraveyard();
-            controls.Player.Move.performed += ctx => characerController.nextMoveCommand = ctx.ReadValue<Vector2>();
-            controls.Player.Move.canceled += ctx => characerController.nextMoveCommand = Vector2.zero;
+            controls.Player.Move.performed += ctx => nextMoveCommand = ctx.ReadValue<Vector2>();
+            controls.Player.Move.canceled += ctx => nextMoveCommand = Vector2.zero;
         }
 
         private void OnEnable()
@@ -30,6 +36,39 @@ namespace Graveyard
         private void Update()
         {
             
+        }
+
+        void FixedUpdate()
+        {
+            if (SceneCamera != null)
+            {
+                Vector2 pos = (Vector2)characterPosition.position + nextMoveCommand * moveSpeed * Time.fixedDeltaTime;
+
+                // Check if the vector is facing up
+                if (nextMoveCommand.y > 0 && Mathf.Approximately(nextMoveCommand.x, 0))
+                {
+                    spriteRenderer.sprite = topFacing;
+                }
+                // Check if the vector is facing left
+                else if (nextMoveCommand.x < 0 && Mathf.Approximately(nextMoveCommand.y, 0))
+                {
+                    spriteRenderer.sprite = leftFacing;
+                    spriteRenderer.flipX = false;
+                }
+                // Check if the vector is facing right
+                else if (nextMoveCommand.x > 0 && Mathf.Approximately(nextMoveCommand.y, 0))
+                {
+                    spriteRenderer.sprite = leftFacing;
+                    spriteRenderer.flipX = true;
+                }
+                // Check if the vector is facing down
+                else //if (nextMoveCommand.y < 0 && Mathf.Approximately(nextMoveCommand.x, 0))
+                {
+                    spriteRenderer.sprite = bottomFacing;
+                }
+
+                characterPosition.position = SceneCamera.RoundToPixel(pos);
+            }
         }
     }
 }
