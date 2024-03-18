91b7602805eb71dee31beae2ee695626e8ae019a
mrsteamfist <christopher.wang@live.com>
2024-03-18 13:12:34 -0700

    Added lantern code and somw of the UI elements

--- Assets/Scripts/ControllerAPI.cs
+++ Assets/Scripts/ControllerAPI.cs
@@ -1,11 +1,22 @@
 using RPGM.Gameplay;
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 namespace Graveyard
 {
+    [ExecuteAlways]
     public class ControllerAPI : MonoBehaviour
     {
+        public enum Facing
+        {
+            Bottom,
+            Left,
+            Right,
+            Top
+        }
+
         public PixelPerfectCamera SceneCamera;
         RPGGraveyard controls;
         private Vector2 nextMoveCommand;
@@ -15,6 +26,18 @@ namespace Graveyard
         public Sprite bottomFacing;
         public Sprite leftFacing;
         public Sprite topFacing;
+        public Transform Lantern;
+        public Facing facing = Facing.Bottom;
+        public bool HasLantern = false;
+        public Vector2 lanternTopPos = new Vector2(0, 1);
+        public Quaternion lanternTopRot = new(90f, 0f, 0f, 0f);
+        public Vector2 lanternBottomPos = new Vector2(0, 1);
+        public Quaternion lanternBottomRot = new(-90f, 0f, 0f, 0f);
+        public Vector2 lanternLeftPos = new Vector2(-1, 0);
+        public Quaternion lanternLeftRot = new(90f, 0f, 0f, 0f);
+        public Vector2 lanternRightPos = new Vector2(0, 1);
+        public Quaternion lanternRightRot = new(90f, 0f, 0f, 0f);
+        public bool ManualSetFacing = false;
 
         private void Awake()
         {
@@ -23,51 +46,110 @@ namespace Graveyard
             controls.Player.Move.canceled += ctx => nextMoveCommand = Vector2.zero;
         }
 
-        private void OnEnable()
+        void OnEnable()
         {
-            controls.Player.Enable();
+            EditorApplication.update += MoveUpdate;
+            controls?.Player.Enable();
         }
 
-        private void OnDisable()
+        private void MoveUpdate()
         {
-            controls.Player.Disable();
+            if (!ManualSetFacing || nextMoveCommand != Vector2.zero)
+            {
+                SetFacing();
+            }
+
+            if (SceneCamera != null)
+            {
+                Vector2 pos = (Vector2)characterPosition.position + nextMoveCommand * moveSpeed * Time.fixedDeltaTime;
+                characterPosition.position = SceneCamera.RoundToPixel(pos);
+            }
+            if (Lantern != null && !HasLantern)
+            {
+                Lantern.gameObject.SetActive(false);
+            }
+
+            if (spriteRenderer != null)
+            {
+                switch (facing)
+                {
+                    case Facing.Left:
+                        spriteRenderer.sprite = leftFacing;
+                        spriteRenderer.flipX = false;
+                        if (Lantern != null && HasLantern)
+                        {
+                            Lantern.transform.rotation = lanternLeftRot;
+                            Lantern.transform.position = lanternLeftPos;
+                        }
+                        break;
+                    case Facing.Right:
+                        spriteRenderer.sprite = leftFacing;
+                        spriteRenderer.flipX = true;
+                        if (Lantern != null && HasLantern)
+                        {
+                            Lantern.transform.rotation = lanternRightRot;
+                            Lantern.transform.position = lanternRightPos;
+                        }
+                        break;
+                    case Facing.Top:
+                        spriteRenderer.sprite = topFacing;
+                        if (Lantern != null && HasLantern)
+                        {
+                            Lantern.transform.rotation = lanternTopRot;
+                            Lantern.transform.position = lanternTopPos;
+                        }
+                        break;
+                    default: // Bottom
+                        spriteRenderer.sprite = bottomFacing;
+                        if (Lantern != null && HasLantern)
+                        {
+                            Lantern.transform.rotation = lanternBottomRot;
+                            Lantern.transform.position = lanternBottomPos;
+                        }
+                        break;
+                }
+            }
         }
 
-        private void Update()
+        static bool IsInbetween(float pos, float small, float large)
         {
-            
+            return pos > small && pos < large;
+        }
+
+        private void SetFacing()
+        {
+            // Check if the vector is facing left
+            if (nextMoveCommand.x < 0 && IsInbetween(nextMoveCommand.y, -.5f, .5f))
+            {
+                facing = Facing.Left;
+            }
+            // Check if the vector is facing right
+            else if (nextMoveCommand.x > 0 && IsInbetween(nextMoveCommand.y, -.5f, .5f))
+            {
+                facing = Facing.Right;
+            }
+            else if (nextMoveCommand.y > 0)
+            {
+                facing = Facing.Top;
+            }
+            // Check if the vector is facing down
+            else //if (nextMoveCommand.y < 0
+            {
+                facing = Facing.Bottom;
+            }
+        }
+
+        void OnDisable()
+        {
+            EditorApplication.update -= MoveUpdate;
+            controls?.Player.Disable();
         }
 
         void FixedUpdate()
         {
             if (SceneCamera != null)
             {
-                Vector2 pos = (Vector2)characterPosition.position + nextMoveCommand * moveSpeed * Time.fixedDeltaTime;
-
-                // Check if the vector is facing up
-                if (nextMoveCommand.y > 0 && Mathf.Approximately(nextMoveCommand.x, 0))
-                {
-                    spriteRenderer.sprite = topFacing;
-                }
-                // Check if the vector is facing left
-                else if (nextMoveCommand.x < 0 && Mathf.Approximately(nextMoveCommand.y, 0))
-                {
-                    spriteRenderer.sprite = leftFacing;
-                    spriteRenderer.flipX = false;
-                }
-                // Check if the vector is facing right
-                else if (nextMoveCommand.x > 0 && Mathf.Approximately(nextMoveCommand.y, 0))
-                {
-                    spriteRenderer.sprite = leftFacing;
-                    spriteRenderer.flipX = true;
-                }
-                // Check if the vector is facing down
-                else //if (nextMoveCommand.y < 0 && Mathf.Approximately(nextMoveCommand.x, 0))
-                {
-                    spriteRenderer.sprite = bottomFacing;
-                }
-
-                characterPosition.position = SceneCamera.RoundToPixel(pos);
+                MoveUpdate();
             }
         }
     }
